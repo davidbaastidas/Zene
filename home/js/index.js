88ec15c57cf42f491e1527529f38b1ed53a75d48
cvinsennau <cande.vinsennau@gmail.com>
2020-05-23 16:47:31 -0300

    intentando hacer js

--- home/js/index.js
+++ home/js/index.js
@@ -1,29 +1,64 @@
-window.addEventListener('load', function() {
+const apiCharts = "https://cors-anywhere.herokuapp.com/https://api.deezer.com/chart/0"
+fetch(apiCharts)
+   .then(function(data){
+      return data.json();
+   })
+   .then(function(datos){
 
-    let inicioSesion = document.querySelector('#inicioSesion');
+      //*TRACKS// 
+      let nameTrack = document.querySelector(".songName");
+      let nameTrackArtist = document.querySelector(".songArtist");
+      let lista = document.querySelector('.lista')
+      let infoTrack = datos.tracks;
 
-    let saludo = document.querySelector('#saludo');
+      console.log(infoTrack.data[0]);
+      
+      for(let i=0; i<5; i++){  
+         
+         nameTrack.innerHTML += infoTrack.data[i].title_short + '<br>'
+         nameTrackArtist.innerHTML += infoTrack.data[i].artist.name;
+      }
 
-    let span = document.querySelector('span');
+      //*ARTISTAS//
 
-    let bienvenida = document.querySelector('.bienvenida');
+      //*ALBUMS//
 
-    let firstSection = document.querySelector('#firstSection');
+   })
+   .catch(function(error){
+      console.log(error)
+   })
 
-    inicioSesion.onload = function() {
-        let nombre = prompt('¿CÓMO TE LLAMÁS?')
-        if (nombre==''){
-           saludo.innerHTML = 'BIENVENID@ USUARIO';
-        } else {
-           saludo.innerHTML = 'BIENVENID@' + nombre;
-           saludo.style.textTransform = "uppercase";
-        }
-        inicioSesion.style.display = "none";
-        span.style.display = "inline";
-     }
 
-     span.onclick= function() {
-        bienvenida.style.display = "none";
-        firstSection.style.display = "flex";
-     }
-})
+
+
+
+
+//    window.addEventListener('load', function() {
+
+//     let inicioSesion = document.querySelector('#inicioSesion');
+
+//     let saludo = document.querySelector('#saludo');
+
+//     let span = document.querySelector('span');
+
+//     let bienvenida = document.querySelector('.bienvenida');
+
+//     let firstSection = document.querySelector('#firstSection');
+
+//     inicioSesion.onload = function() {
+//         let nombre = prompt('¿CÓMO TE LLAMÁS?')
+//         if (nombre==''){
+//            saludo.innerHTML = 'BIENVENID@ USUARIO';
+//         } else {
+//            saludo.innerHTML = 'BIENVENID@' + nombre;
+//            saludo.style.textTransform = "uppercase";
+//         }
+//         inicioSesion.style.display = "none";
+//         span.style.display = "inline";
+//      }
+
+//      span.onclick= function() {
+//         bienvenida.style.display = "none";
+//         firstSection.style.display = "flex";
+//      }
+// })
