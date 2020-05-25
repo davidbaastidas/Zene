768c891e0018f75fbe0a204db3b04cd890d470db
cvinsennau <cande.vinsennau@gmail.com>
2020-05-25 13:05:42 -0300

    generos incompleto

--- generos/js/generos/genero.js
+++ generos/js/generos/genero.js
@@ -2,3 +2,26 @@ window.addEventListener("load", function(){
     let loader = document.querySelector(".loader-wrapper");
     loader.className += " hidden";
 })
+
+let genre = "https://cors-anywhere.herokuapp.com/https://api.deezer.com/genre"
+fetch (genre)
+    .then(function(response){
+        return response.json();
+    })
+    .then(function(datos){
+        
+        console.log(datos.data);
+        
+        let genres = datos.data
+        let lista = document.querySelector (".generos");
+
+        genres.forEach (function(listaGeneros){
+            lista.innerHTML += '<li>' + listaGeneros.name + '</li>' ;
+
+        })
+
+
+    })
+    .catch(function(error){
+        console.log(error);
+    })
