b8f3d518a2a446d1b6b808d5a1f113bf3e63c50f
davidbaastidas <dbastidasvelez@udesa.edu.ar>
2020-05-31 20:39:35 -0300

    avance con el js de resultados, y linkee con otras paginas para que se respete el ID en el URL

--- resultados/js/resultados.js
+++ resultados/js/resultados.js
@@ -22,9 +22,9 @@ fetch(url)
         console.log(datos);
         let resultados = datos.data;
 
-        resultados.forEach(function(resultados){
-            modificacion.innerHTML += '<img src="' + resultados.album.cover_medium + '">' + '<h2>' + resultados.title_short + '</h2>' + '<h3>' + resultados.artist.name + '</h3>'
-        });
+        for(let i=0; i<resultados.length; i++){
+            modificacion.innerHTML += '<a href=../album/album.html?id=' + resultados[i].album.id + '>' + '<img src="' + resultados[i].album.cover_medium + '">' + '</a>' + '<a href=../album/album.html?id=' + resultados[i].album.id + '>' +'<h2>' + 'Titulo Album: ' + resultados[i].album.title + '</h2>' + '</a>' + '<a href=../artist/artist.html?id=' + resultados[i].artist.id + '>' + '<h3>' + 'Artista: ' + resultados[i].artist.name + '</h3>' + '</a>'
+        }
     })
     .catch(function(error){
         console.log(error);
