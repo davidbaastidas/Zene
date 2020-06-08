17afcafb959516a85af2eda52c9339115f88b397
davidbaastidas <dbastidasvelez@udesa.edu.ar>
2020-06-08 17:31:19 -0300

    agregue logos en los headers y empece con una idea en el login form

--- login/js/login.js
+++ login/js/login.js
@@ -1,37 +1,41 @@
-//capturo los elementos
-let loader = document.querySelector('.loader-wrapper');
+//PASOS PARA AGREGAR ELEMENTO A LA PLAYLIST
+//Paso 1: Obtener informacion del storage, ver que hay almacenado en el. Recuperar datos del storage
 
-let registroForm = document.querySelector('.registroForm');
+let recuperoStorage = localStorage.getElementsByName('user');
+//Si todavia no tengo tracks en mi playlist
+if(recuperoStorage == null){
+    //Creo una lista vacia
+    user = [];
+} else {
+    //Recupero el array de localStorage
+    user = JSON.parse(recuperoStorage);
+}
 
-let registro = document.querySelector('.registro');
+//Me fijo que no este en la lista y cambio texto del boton
+if(user.includes("")){
+    document.querySelector('.boton').innerHTML = "INICIAR ZENE";
+}
+
+//Paso 2: agregar un track a la playlist
+let agregar = document.querySelector('.boton');
+
+agregar.onclick = function(){
+    //Agrego el ID del track a la lista
+    if(user.includes("")){
+        //Si el tracj esta, tenemos que quitarlo
+        let indiceEnElArray = user.indexOf("");
+        user.splice(indiceEnElArray, 1);
+        document.querySelector('.boton').innerHTML = "INICIAR ZENE"
+        console.log(user);
+        
+    } else {
+        user.push("");
+        //Pongo el "Quitar a la lista"
+        document.querySelector('.boton').innerHTML = "INICIAR ZENE"
+    }
+    //Paso 3: guardar la lista en el localStorage
+    let userParaStorage = JSON.stringify(user);
+    sessionStorage.setItem('user', userParaStorage);
+    console.log(localStorage);
     
-let loginForm = document.querySelector('.loginForm');
-
-let volver = document.querySelector('.volver');
-
-let formulario = document.querySelector('.form');
-
-let usuario = document.querySelector('[name=usuario]');
-
-let contras = document.querySelector('[name=contras]');
-
-let mail = document.querySelector('[name=mail]');
-
-let aviso = document.querySelector('.aviso');
-
-let boton = document.querySelector('.boton');
-
-window.addEventListener("load", function(){
-    loader.className += " hidden";
-})
-
-registro.onclick = function(){
-    loginForm.style.display = "none";
-    registroForm.style.display = "inherit";
 }
-
-volver.onclick = function(){
-    registroForm.style.display = "none";
-    loginForm.style.display = "inherit";
-}
-
