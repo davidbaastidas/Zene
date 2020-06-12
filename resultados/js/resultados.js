24c70df48363e57494c49ef56782ff102b64e07e
cvinsennau <cande.vinsennau@gmail.com>
2020-06-12 10:13:48 -0300

    resultados

--- resultados/js/resultados.js
+++ resultados/js/resultados.js
@@ -14,6 +14,15 @@ console.log(searchParams);
 let search = searchParams.get('search');
 console.log(search);
 
+let resultadoSearch = document.querySelector("#resultadoBusqueda");
+
+if(search == null || search == "[]"){
+    search = [];
+    resultadoSearch.innerHTML = 'No se encontraron resultados para tu búsqueda';
+} else {
+    resultadoSearch.innerHTML = 'Los resultados de tu busqueda fueron...'
+}
+
 let proxy = "https://cors-anywhere.herokuapp.com/";
 let url = proxy + "https://api.deezer.com/search?q=" + search;
 
@@ -32,7 +41,7 @@ fetch(url)
         } else {
 
             for(let i=0; i<resultados.length; i++){
-                modificacion.innerHTML += '<li class="listas">' + '<div>'+'<a href=../album/album.html?id=' + resultados[i].album.id + '>' + '<img src="' + resultados[i].album.cover_xl + '">' + '</a>' + '</div>' +'<a id="route1" href=../album/album.html?id=' + resultados[i].album.id + '>' +'<h2>' + 'Album: ' + resultados[i].album.title + '</h2>' + '</a>' + '<a id="route2" href=../artist/artist.html?id=' + resultados[i].artist.id + '>' + '<h3>' + 'Artista: ' + resultados[i].artist.name + '</h3>' + '</a>';
+                modificacion.innerHTML += '<li class="listas">' + '<div>'+'<a href=../album/album.html?id=' + resultados[i].album.id + '>' + '<img src="' + resultados[i].album.cover_xl + '">' + '</a>' + '</div>' + '<a id="route0" href=../tracks/tracks.html?id=' + resultados[i].id + '>' +'<h2 id="trackName">' + resultados[i].title_short + '</h2>' + '</a>' + '<a id="route2" href=../artist/artist.html?id=' + resultados[i].artist.id + '>' + '<h2>' + resultados[i].artist.name + '</h2>' + '</a>' + '</a>' + '<a id="route1" href=../album/album.html?id=' + resultados[i].album.id + '>' +'<h2>' + 'Album: ' + resultados[i].album.title + '</h2>' ;
             }
 
         }
